6e66d496faa2cbb1cb7953191793cb5932d512b2
diunkz <noahdiunkz@gmail.com>
2025-06-22 19:47:30 -0400

    fixing errors

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ SWAGGER = {
     'title': 'Catálogo de Receitas Gourmet',
     'uiversion': 3
 }
-SQLALCHEMY_DATABASE_URI = 'sqlite://recipes.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///recipes.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_SECRET_KEY = 'sua_chave_jwt_secreta'
--- gourmet_recipes.py
+++ gourmet_recipes.py
@@ -11,9 +11,9 @@ print(['SQLALCHEMY_DATABASE_URI'])
 print(['SWAGGER'])
 print(['CACHE_TYPE'])
 
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullale=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
 class Recipe(db.Model):
