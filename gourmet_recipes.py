19da9fe7a372378697f3c16e176f7b36b0274c02
diunkz <noahdiunkz@gmail.com>
2025-06-22 21:11:12 -0400

    creating routes /login and /register

--- gourmet_recipes.py
+++ gourmet_recipes.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import (
+    JWTManager, create_access_token,
+    jwt_required, get_jwt_identity
+)
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+jwt = JWTManager(app) 
 
 print(app.config['SECRET_KEY'])
-print(['SQLALCHEMY_DATABASE_URI'])
-print(['SWAGGER'])
-print(['CACHE_TYPE'])
+print(app.config['SQLALCHEMY_DATABASE_URI'])
+print(app.config['SWAGGER'])
+print(app.config['CACHE_TYPE'])
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +26,84 @@ class Recipe(db.Model):
     title = db.Column(db.String(120), nullable=False)
     ingredients = db.Column(db.Text, nullable=False)
     time_minutes = db.Column(db.Integer, nullable=False) 
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    """"
+    Registra um novo usuário.
+    ---
+    parameters:
+    -   in: body
+        name: body
+        required: true
+        schema:
+            type: object
+            properties:
+                username:
+                    type: string
+                password:
+                    type: string
+    responses:
+        201:
+            description: Usuário criado com sucesso
+        400:
+            description: Usuário já existe
+    """
+
+    data = request.get_json()
+
+    if User.query.filter_by(username=data['username']).first():
+        return jsonify({"error": "User already exists"}), 400
+
+    new_user = User(username=data['username'], password=data['password'])
+    
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"msg": "User created"}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    """"
+    Faz o login do usuário e retorna um JWT.
+    ---
+    parameters:
+    -   in: body
+        name: body
+        required: true
+        schema:
+            type: object
+            properties:
+                username:
+                    type: string
+                password:
+                    type: string
+    responses:
+        200:
+            description: Login bem sucedido, retorna JWT
+        401:
+            description: Credenciais Inválidas
+    """
+
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+
+    if user and user.password == data['password']:
+        # Converte o ID para string
+        token = create_access_token(identity=str(user.id))
+
+        return jsonify({"acess_token": token}), 200
+
+    return jsonify({"error": "Invalid credentials"}), 401
+
+
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identity() # Retorna o 'identity' usado na 
+                                         # criação do token
+
+    return jsonify({"msg": f"Usuário com ID {current_user_id} acessou a rota protegida"}), 200                                   
 
 if __name__ == '__main__':
     with app.app_context():
